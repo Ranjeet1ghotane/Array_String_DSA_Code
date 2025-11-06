555589c0b16cbaae1d5f9cd26eac9cc166c58edf
Ranjeet1ghotane <ranjeetghotane2002@gmail.com>
2025-11-06 22:56:05 +0530

    Commiting code

---
+++ DSA/LinearSearch.java
@@ -0,0 +1,19 @@
+public  class LinearSearch{
+    public int linearSearch(int[]arr , int target){
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == target){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {89,8,42,5,7,1,4,7,1};
+        int target = 5;
+
+        LinearSearch li = new LinearSearch();
+        int res = li.linearSearch(arr, target);
+        System.out.println(res);
+
+    }
+}

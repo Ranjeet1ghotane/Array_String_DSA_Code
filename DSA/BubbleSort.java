0dc22d976028fd89f2b47df36e1db1590c46d36b
Ranjeet1ghotane <ranjeetghotane2002@gmail.com>
2025-11-08 13:33:05 +0530

    Commiting BubbleSort.java

--- DSA/BubbleSort.java
+++ DSA/BubbleSort.java
@@ -2,11 +2,10 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public int[] bubbleSort(int[] arr) {
-        
 
         // Bubble sort algorithm
-        for (int i = 0; i < arr.length-1; i++) {
-            for (int j = 0; j < arr.length-1; j++) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     // Swap arr[j] and arr[j+1]
                     int temp = arr[j];
@@ -19,7 +18,7 @@ public class BubbleSort {
     }
 
     public static void main(String[] args) {
-        int[] arr = {5, 1, 4, 2, 8};
+        int[] arr = { 5, 1, 4, 2, 8 };
         BubbleSort bu = new BubbleSort();
         int[] res = bu.bubbleSort(arr);
         System.out.println(Arrays.toString(res));

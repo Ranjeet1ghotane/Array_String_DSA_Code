44c9889b98eb00f4a8bdec1cf5cd31d69b18da60
Ranjeet1ghotane <ranjeetghotane2002@gmail.com>
2025-11-11 19:25:59 +0530

    Updating FirstCharWhichNotReapet.java

--- FirstCharWhichNotReapet.java
+++ FirstCharWhichNotReapet.java
@@ -1,11 +1,11 @@
 public class FirstCharWhichNotReapet {
     public char firstChar( String str){
         
-        char ch2 = '1';
+        char ch2 = '0';
         for(int i = 0; i < str.length(); i++){
             char ch1 = str.charAt(i);
             for(int j = i+1; j < str.length(); j++){
-                if(ch1 != str.charAt(j)){
+                if(ch1 == str.charAt(j)){
                     return ch1;
                 }
             }
